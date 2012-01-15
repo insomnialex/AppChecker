009686b4fc86de088613e97fc667b27f931cb629
unknown <alex@monstermedia.net>
2012-01-15 14:33:22 -0500

    first commit - adding project

---
+++ ExampleApp.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ExampleApp
+{
+    partial class ExampleApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExampleApp));
+            this.listBox_log = new System.Windows.Forms.ListBox();
+            this.textBox_application = new System.Windows.Forms.TextBox();
+            this.button_select = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox_log
+            // 
+            this.listBox_log.FormattingEnabled = true;
+            this.listBox_log.HorizontalScrollbar = true;
+            this.listBox_log.Location = new System.Drawing.Point(1, 28);
+            this.listBox_log.Name = "listBox_log";
+            this.listBox_log.Size = new System.Drawing.Size(620, 95);
+            this.listBox_log.TabIndex = 0;
+            // 
+            // textBox_application
+            // 
+            this.textBox_application.Location = new System.Drawing.Point(1, 2);
+            this.textBox_application.Name = "textBox_application";
+            this.textBox_application.ReadOnly = true;
+            this.textBox_application.Size = new System.Drawing.Size(390, 20);
+            this.textBox_application.TabIndex = 2;
+            // 
+            // button_select
+            // 
+            this.button_select.Location = new System.Drawing.Point(397, 2);
+            this.button_select.Name = "button_select";
+            this.button_select.Size = new System.Drawing.Size(75, 23);
+            this.button_select.TabIndex = 3;
+            this.button_select.Text = "select";
+            this.button_select.UseVisualStyleBackColor = true;
+            this.button_select.Click += new System.EventHandler(this.button_select_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(471, 2);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(75, 23);
+            this.button_clear.TabIndex = 4;
+            this.button_clear.Text = "clear";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(546, 2);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(75, 23);
+            this.button_stop.TabIndex = 5;
+            this.button_stop.Text = "stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // ExampleApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 124);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_select);
+            this.Controls.Add(this.textBox_application);
+            this.Controls.Add(this.listBox_log);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ExampleApp";
+            this.Text = " - App Checker • alexquintero.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_log;
+        private System.Windows.Forms.TextBox textBox_application;
+        private System.Windows.Forms.Button button_select;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Button button_stop;
+    }
+}
+
---
+++ ExampleApp.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ExampleApp
+{
+    public partial class ExampleApp : Form
+    {
+        private System.Timers.Timer CheckAppTimer;
+        private string HomeDir;
+        private bool Running;
+
+        public ExampleApp()
+        {
+            InitializeComponent();
+
+            WindowState = FormWindowState.Minimized;
+
+            System.Reflection.Assembly currentExe = System.Reflection.Assembly.GetExecutingAssembly();
+            HomeDir = Path.GetDirectoryName(currentExe.Location);
+
+            LoadConfig();
+            Running = true;
+
+            CheckAppTimer = new System.Timers.Timer(500);
+            CheckAppTimer.Elapsed += new System.Timers.ElapsedEventHandler(CheckAppTimer_Elapsed);
+            CheckAppTimer.Start();
+        }
+                
+        void CheckAppTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            CheckProcess();
+        }
+
+        /// <summary>
+        /// Select application to watch
+        /// </summary>
+        private void SelectApplication() 
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                textBox_application.Text = ofd.FileName;
+                SaveConfig(ofd.FileName);
+            }
+        }
+
+        /// <summary>
+        /// Checks for process indicated in textbox is running if the global variable Running is set to true
+        /// </summary>
+        private void CheckProcess() 
+        {
+            // if Running is set to true, check for process
+            if (Running)
+            {
+                if (!String.IsNullOrEmpty(textBox_application.Text))
+                {
+                    string fullPath = textBox_application.Text;
+                    // get the name of the application to check for from the filepath
+                    string app = Path.GetFileNameWithoutExtension(fullPath);
+                    // get a list of processes that match the application name and attempt to start
+                    // if none found
+                    if (Process.GetProcessesByName(app).Length < 1)
+                    {
+                        UpdateLogAsync("process missing, attempting to start");
+                        try
+                        {
+                            Process proc = new Process();
+                            proc.StartInfo.FileName = fullPath;
+                            proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(fullPath);
+                            proc.Start();
+                            UpdateLogAsync("process successfully restarted");
+                        }
+                        catch (Exception ex)
+                        {
+                            UpdateLogAsync("error restarting application: " + ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Toggle the global vairable Running, then change the text on the button
+        /// </summary>
+        private void ToggleStop() 
+        {
+            if (Running)
+            {
+                UpdateLogAsync("stopping process check");
+                Running = false;
+                button_stop.Text = "start";
+            }
+            else 
+            {
+                UpdateLogAsync("starting process check");
+                Running = true;
+                button_stop.Text = "stop";
+            }
+        }
+        
+        private void ClearApp() 
+        {
+            textBox_application.Clear();
+        }
+        
+        #region config file
+        
+        /// <summary>
+        /// Attempt to load configuration file from working directory
+        /// </summary>
+        private void LoadConfig() 
+        {            
+            try
+            {
+                TextReader sr = new StreamReader(HomeDir + "//config");
+                textBox_application.Text = sr.ReadToEnd();
+                sr.Close();
+                UpdateLogAsync("config file found");
+            }
+            catch { UpdateLogAsync("config file not found"); }
+            
+        }
+
+        /// <summary>
+        /// Attempt to save config file to working directory
+        /// </summary>
+        /// <param name="text">File path of application to check</param>
+        private void SaveConfig(string text)
+        {
+            try
+            {
+                TextWriter tr = new StreamWriter(HomeDir + "/config");
+                tr.Write(text);
+                tr.Close();
+                UpdateLogAsync("saved config");
+            }
+            catch { UpdateLogAsync("error saving config"); }
+ 
+        }
+
+        #endregion config file
+
+        #region log window code
+
+        /// <summary>
+        /// Delegate for adding text item to listbox
+        /// </summary>
+        /// <param name="text">Text to add to listbox at position 0</param>
+        delegate void UpdateLogDelegate(string text);
+
+        /// <summary>
+        /// Add a text item to first position of listbox
+        /// </summary>
+        /// <param name="text">Text to add to listbox</param>
+        private void UpdateLog(string text)
+        {
+            listBox_log.Items.Insert(0, text);
+        }
+
+        /// <summary>
+        /// Threadsafe function to add a text item to the first position of listbox
+        /// </summary>
+        /// <param name="text">Text to add to listbox</param>
+        private void UpdateLogAsync(string text)
+        {
+            // if attemping to access the listbox form a separate thread, use delegate
+            if (InvokeRequired)
+            {
+                // catch any errors that may occur while attempting to update listbox 
+                // (ex. ObjectDisplosedException)
+                try
+                {
+                    UpdateLogDelegate uld = new UpdateLogDelegate(UpdateLog);
+                    Invoke(uld, text);
+                }
+                catch { }
+            }
+            // else, update listbox directly
+            else
+                UpdateLog(text); 
+        }
+
+        #endregion log window code
+
+        /* GUI hooks code below */
+
+        private void button_select_Click(object sender, EventArgs e)
+        {
+            SelectApplication();
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            ClearApp();
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            ToggleStop();
+        }
+    }
+}
